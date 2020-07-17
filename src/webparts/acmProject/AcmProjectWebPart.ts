29b8784a7797c8692032dd0ade88adf2629adfd0
Madhuri-Chunduri <madhuri.c@technovert.net>
2020-07-17 13:25:59 +0530

    Initial Setup

---
+++ src/webparts/acmProject/AcmProjectWebPart.ts
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+
+import * as strings from 'AcmProjectWebPartStrings';
+import AcmProject from './components/AcmProject';
+import { IAcmProjectProps } from './components/IAcmProjectProps';
+
+export interface IAcmProjectWebPartProps {
+  description: string;
+}
+
+export default class AcmProjectWebPart extends BaseClientSideWebPart <IAcmProjectWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<IAcmProjectProps> = React.createElement(
+      AcmProject,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/acmProject/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IAcmProjectWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'AcmProjectWebPartStrings' {
+  const strings: IAcmProjectWebPartStrings;
+  export = strings;
+}
